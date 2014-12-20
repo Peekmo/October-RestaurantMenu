b3e98fadf52287b12cabc7e75925e0cb5cf7a931
Peekmo <axel.anceau@gmail.com>
2014-12-20 09:13:14 +0100

    Added backend menu & permission

--- Plugin.php
+++ Plugin.php
@@ -2,14 +2,17 @@
 
 namespace Peekmo\RestaurantMenu;
 
+use Backend;
+
 class Plugin extends \System\Classes\PluginBase
 {
-	public function pluginDetails() 
+	public function pluginDetails()
 	{
 		return [
 			'name' => 'Restaurant Menu Plugin',
 			'description' => 'Provides some tools to manage a restaurant menu',
-			'author' => 'Axel Anceau (Peekmo)'
+			'author' => 'Axel Anceau (Peekmo)',
+			'icon' => 'icon-leaf'
 		];
 	}
 
@@ -17,6 +20,37 @@ class Plugin extends \System\Classes\PluginBase
 	{
 		return [
 			'Peekmo\RestaurantMenu\Components\RestaurantMenu' => 'peekmo_rsmenu'
+		];
+	}
+
+	public function registerPermissions()
+	{
+		return [
+			'peekmo.rsmenu' => ['label' => 'Manage menus for the restaurant']
+		];
+	}
+
+	public function registerNavigation()
+	{
+		return [
+			'Restaurant' => [
+				'label' => 'Restaurant',
+				'url' => Backend::url('peekmo/rsmenu'),
+				'permissions' => ['peekmo.rsmenu'],
+				'order' => 500,
+				'sideMenu' => [
+					'menus' => [
+						'label' => 'Menus',
+						'url'   => Backend::url('peekmo/rsmenu/menus'),
+						'permissions' => ['peekmo.rsmenu']
+					],
+					'offers' => [
+						'label' => 'Special offers',
+						'url'   => Backend::url('peekmo/rsmenu/offers'),
+						'permissions' => ['peekmo.rsmenu']
+					]
+				]
+			]
 		];
 	}
 }
